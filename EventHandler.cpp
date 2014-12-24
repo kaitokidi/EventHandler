613f2449932856d4c33ca5f2e49c1008a95a5957
Kaito <kaaitokid@gmail.com>
2014-12-24 01:44:00 +0100

    Update

--- EventHandler.cpp
+++ EventHandler.cpp
@@ -2,6 +2,23 @@
 
 	EventHandler::EventHandler(){
 		
+		int qtty = (int)sf::Keyboard::KeyCount;
+		actionActivated_k = std::vector<bool> (qtty, false);
+		actionKey_k = std::vector<sf::Keyboard::Key> (qtty, sf::Keyboard::Unknown);
+		
+		dirKeys = std::vector<sf::Keyboard::Key> (4);
+		dirKeys[0] = sf::Keyboard::Up; dirKeys[1] = sf::Keyboard::Down;
+		dirKeys[2] = sf::Keyboard::Right; dirKeys[3] = sf::Keyboard::Left;
+		
+		edirKeys = std::vector<sf::Keyboard::Key> (4);
+		edirKeys[0] = sf::Keyboard::W;	edirKeys[1] = sf::Keyboard::S;
+		edirKeys[2] = sf::Keyboard::D;	edirKeys[3] = sf::Keyboard::A;
+		edirKeys[4] = sf::Keyboard::E;	edirKeys[5] = sf::Keyboard::Q;
+		edirKeys[6] = sf::Keyboard::C;	edirKeys[7] = sf::Keyboard::Z;
+		
+		fireKey_k = sf::Keyboard::F;
+		jumpKey_k = sf::Keyboard::Space;
+
 	}
 	
 	/** 
@@ -9,9 +26,78 @@
 	 * w -> renderWindow from which you want to handle the events;
 	 */
 	void EventHandler::handleEvents(sf::RenderWindow& w){
-		
+		sf::Event event;
+		while(w.pollEvent(event)){
+			handleEvent(event);
+		}	
+	}
+
+	void EventHandler::handleEvent(sf::Event event){ 
+		switch (event.type){
+				//KeyPressed event
+				case  sf::Event::KeyPressed:
+					
+					//basic movement
+					if (event.key.code == dirKeys[up])    { k_dir = up; }
+					if (event.key.code == dirKeys[down])  { k_dir = down; }
+					if (event.key.code == dirKeys[left])  { k_dir = left; }
+					if (event.key.code == dirKeys[right]) { k_dir = right; }
+					
+					//extended movement
+					if (event.key.code == edirKeys[N])  { k_edir = N; }
+					if (event.key.code == edirKeys[S])  { k_edir = S; }
+					if (event.key.code == edirKeys[E])  { k_edir = E; }
+					if (event.key.code == edirKeys[W])  { k_edir = W; }
+					if (event.key.code == edirKeys[NE]) { k_edir = NE; }
+					if (event.key.code == edirKeys[NW]) { k_edir = NW; }
+					if (event.key.code == edirKeys[SE]) { k_edir = SE; }
+					if (event.key.code == edirKeys[SW]) { k_edir = SW; }
+					
+					//fire
+					if (event.key.code == fireKey_k) { fireActivated = true; }
+					
+					//jump 
+					if (event.key.code == jumpKey_k) { jumpActivated = true; }
+
+					//actions
+					for(int i = 0; i < actionKey_k.size(); ++i) {
+						if (event.key.code == actionKey_k[i]) { 
+								actionActivated_k[i] = true;
+						}
+					}
+					
+				break;
+				
+				//Default
+				default:
+					//Do nothing
+					break;
+			}
 	}
 	
-	void EventHandler::setDirectionKeys(std::vector<sf::Keyboard::Key> keys, int id){
-		
-	}
+	void EventHandler::setDirectionKeys(std::vector<sf::Keyboard::Key> keys, int id){ dirKeys = keys; }
+	
+	void EventHandler::setExtendedDirectionKeys(std::vector<sf::Keyboard::Key> keys, int id){ edirKeys = keys; }
+
+	
+	void EventHandler::setFireKey_j(int but){ fireBut_j = but; }
+	
+	void EventHandler::setFireKey_k(sf::Keyboard::Key key){ fireKey_k = key; }
+	
+	void EventHandler::setFireKey_m(sf::Mouse::Button but){ fireBut_m = but; }
+
+	
+	void EventHandler::setJumpKey_j(int but){ jumpBut_j = but; }
+	
+	void EventHandler::setJumpKey_k(sf::Keyboard::Key key){ jumpKey_k = key; }
+	
+	void EventHandler::setJumpKey_m(sf::Mouse::Button but){ fireBut_m = but; }
+
+	
+	void EventHandler::setActionKey_j(int but, int id){ actionBut_j[id] = but; }
+	
+	void EventHandler::setActionKey_k(sf::Keyboard::Key key, int id){ actionKey_k[id] = key; }
+	
+	void EventHandler::setActionKey_m(sf::Mouse::Button but, int id){ actionBut_m[id] = but; }
+	
+	
--- EventHandler.h
+++ EventHandler.h
@@ -7,8 +7,8 @@
 #include <SFML/System.hpp>
 #include <SFML/Graphics.hpp>
 
-enum dir {up, down, left, right, no_dir};
-enum edir {N, NE, E, SE, S, SW, W, no_edir};
+enum dir  {up, down, left, right, no_dir};
+enum edir {N, S, E, W, NE, NW, SE, SW, no_edir};
 
 class EventHandler {
 
@@ -22,25 +22,70 @@ class EventHandler {
 		
 		/** 
 		* Function that will handle the events for the given window 'w'
-		* w -> renderWindow from which you want to handle the events;
+		* 	w -> renderWindow from which you want to handle the events;
 		*/
 		void handleEvents(sf::RenderWindow& w);
 		
+		/** 
+		* Function that will update the variables if the event requieres it.
+		*/
+		void handleEvent(sf::Event e);
+		
 		/**
-		 * Function to set which keys do you want to use for your keyboard
+		 * Function to set which keys do you want to use for the directions
+		 * control (only up down right left) on your keyboard
 		 * and the id you will use to take their values, by default it will
-		 * use W A S D which will be the id = 0;
-		 * keys -> a vector with the four keys using sf::Keyboard::Key, 
-		 * in the order up, down, left, right
-		 * id -> integer that will discrimine which keyboard are you using
+		 * use W A S D and id = 0;
+		 * 	keys -> a vector with the four keys using sf::Keyboard::Key, 
+		 * 		in the order: up, down, left, right
+		 * 	id -> integer that will discrimine which keyboard are you using
 		 */
 		void setDirectionKeys(std::vector<sf::Keyboard::Key> keys, int id);
+		
+		/**
+		 * Function to set which keys do you want to use for the directions
+		 * control (up down right left and diagonals) on your keyboard
+		 * and the id you will use to take their values, by default it will
+		 * use W A S D and id = 0;
+		 * 	keys -> a vector with the four keys using sf::Keyboard::Key, 
+		 * 		in the order: up, down, left, right
+		 * 	id -> integer that will discrimine which keyboard are you using
+		 */ 
+		void setExtendedDirectionKeys(std::vector<sf::Keyboard::Key> keys, int id);
+		
+		/**
+		 * Function to set the Fire button of the joystick
+		 * 	but -> the number of the button you want to use
+		 * 		(it depends on the joystick so check it out)
+		 */
+		void setFireKey_j(int but);
+		
+		/**
+		 * Function to set the Fire key of the keyboard
+		 * 	key -> the kay you want to use to fire in the form sf::Keyboard::Key key
+		 */
+		void setFireKey_k(sf::Keyboard::Key key);
+		
+		/**
+		 * Function to set the Fire button of the mouse
+		 * 	but -> the button you want to use to fire in the form sf::Mouse::Button but
+		 */
+		void setFireKey_m(sf::Mouse::Button but);
+		
+		void setJumpKey_j(int but);
+		void setJumpKey_k(sf::Keyboard::Key key);
+		void setJumpKey_m(sf::Mouse::Button but);
+		
+		void setActionKey_j(int but, int id);
+		void setActionKey_k(sf::Keyboard::Key key, int id);
+		void setActionKey_m(sf::Mouse::Button but, int id);
+		
 		
 		//getters
 		//setXbuttonk_m_j
 		
 		
-	private:
+	private:		
 
 		dir k_dir;
 		dir m_dir;
@@ -49,7 +94,27 @@ class EventHandler {
 		edir k_edir;
 		edir m_edir;
 		edir j_edir;
-	
+
+		int fireBut_j;
+		int jumpBut_j;
+		std::vector<int> actionBut_j;
+		
+		sf::Keyboard::Key fireKey_k;
+		sf::Keyboard::Key jumpKey_k;
+		std::vector<bool> actionActivated_k;
+		std::vector<sf::Keyboard::Key> actionKey_k;
+		
+		sf::Mouse::Button fireBut_m;
+		sf::Mouse::Button jumpBut_m;		
+		std::vector<bool> actionActivated_m;
+		std::vector<sf::Mouse::Button> actionBut_m;
+		
+		bool haveFocus;
+		bool fireActivated;
+		bool jumpActivated;
+		
+		std::vector<sf::Keyboard::Key> dirKeys;
+		std::vector<sf::Keyboard::Key> edirKeys;
 	
 };
 
